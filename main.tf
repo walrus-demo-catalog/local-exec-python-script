e7b406b5918ad582f66564aba8d5f5fe3ddae775
Yinlin Li <yinlin@seal.io>
2024-02-05 17:59:17 +0800

    fix: duplicate command

--- main.tf
+++ main.tf
@@ -1,12 +1,9 @@
 resource "null_resource" "local" {
   provisioner "local-exec" {
-    command = "python3"
-
     environment = {
       PYTHON_SCRIPT_CONTENT = var.python_script_content
     }
 
-    interpreter = ["bash", "-c"]
     command = <<-EOT
       echo "$PYTHON_SCRIPT_CONTENT" > script.py
       python3 script.py
